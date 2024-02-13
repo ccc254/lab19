43ee509cbd0cfe41663636a293d7aca0ec67c2f8
Chanon <natchanon.thamkankang@gmail.com>
2024-02-13 16:40:22 +0700

    complete

--- lab19.h
+++ lab19.h
@@ -66,7 +66,42 @@ void Unit::newTurn(){
 
 
 /////////////////////////////////////////////////////////////////////////////////////
-//Write function members isDead(), guard(), heal(), beAttacked(), and attack() here//
+bool Unit::isDead(){
+    if(hp <= 0){
+    return true;
+    }
+    else{
+    return false;
+    }
+}
+void Unit::guard(){
+    guard_on = true;
+}
+int Unit::beAttacked(int oppatk){
+    int dam;
+    if(guard_on){
+    dam = (oppatk - def)/3;
+    hp -= dam;
+    }else {
+    dam = oppatk - def;
+    hp -= dam;
+    }
+    return dam;
+}
+int Unit::attack(Unit &x){
+	return x.beAttacked(atk);
+}
+int Unit::heal(){
+    int h;
+    h = rand()%21 +10;
+    if(hp + h > hpmax){
+        h = hpmax - hp;
+        hp += h;
+    }else{
+        hp += h;
+    }
+    return h;
+}
 /////////////////////////////////////////////////////////////////////////////////////
 
 
